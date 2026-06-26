bd7006c16a723f28baf2adcb9f097efdd492c828
nguyenthingocthaofw-star <email_cua_nick_moi@gmail.com>
2026-06-26 23:47:47 +0700

    Print-Function

---
+++ Introduction/print-function.py
@@ -0,0 +1,4 @@
+if __name__ == '__main__':
+    n = int(input())
+    for i in range(1,n+1):
+        print(i, end="") #Trong python sẽ tự động xuống dòng sau khi in xong nội dung nên dùng end="" có nghĩa kết thúc print là một chuỗi rỗng để các số liền nhau
