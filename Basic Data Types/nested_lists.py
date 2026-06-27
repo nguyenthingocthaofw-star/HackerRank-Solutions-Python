bf2630eba19ed0646b7c2d8fb6de1ff4bd6f705d
nguyenthingocthaofw-star <nguyenthingocthao.fw@gmail.com>
2026-06-27 22:05:56 +0700

    Thêm comment

--- Basic Data Types/nested_lists.py
+++ Basic Data Types/nested_lists.py
@@ -12,7 +12,7 @@ if __name__ == '__main__':
     second_lowest_score = unique_scores[1]
     # Dùng List Comprehension để lọc ra tên các bạn có điểm bằng mức điểm này
     runner_up_names = [student[0] for student in students if student[1] == second_lowest_score]
-    # Sắp xếp tên theo thứ tự chữ cái (A-Z) và in ra từng dòng
+    # Sắp xếp tên theo thứ tự chữ cái và in ra từng dòng
     runner_up_names.sort()
     for name in runner_up_names:
         print(name)
